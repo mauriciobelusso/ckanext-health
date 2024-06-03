b4901f0c09c5d8f99de93dcc961e5b978a1878bc
Mauricio Belusso <mauriciobelusso@gmail.com>
2024-06-03 01:10:16 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setup(
     name='ckanext-health',
     version='0.1',
     description='CKAN Health Monitoring Extension',
-    author='Seu Nome',
-    author_email='seuemail@example.com',
-    url='https://github.com/seuusuario/ckanext-health',
+    author='Mauricio Belusso',
+    author_email='mauriciobelusso@gmail.com',
+    url='https://github.com/mauriciobelusso/ckanext-health',
     packages=find_packages(),
     install_requires=[
         'ckan'
